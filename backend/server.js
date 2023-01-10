c773ac5befb89e5e645ea78e8548c8124cebc6c3
Junior Oyewunmi <junioroyewunmi@Juniors-MacBook-Air.local>
2023-01-11 00:07:18 +0100

    use route in server.js seed sample product and created product model and seed route

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,12 @@ import express from "express";
 import data from "./data.js";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import seedRouter from "./routes/seedRoutes.js";
+import productRouter from "./routes/productRoutes.js";
 
 dotenv.config();
 
-mongoose.set("strictQuery", false);
+// mongoose.set("strictQuery", true);
 mongoose
   .connect(process.env.MONGODB_URL)
   .then(() => {
@@ -16,29 +18,14 @@ mongoose
   });
 
 const app = express();
-// test
-app.get("/api/products", (req, res) => {
-  res.send(data.products);
-});
+app.use("/api/seed", seedRouter);
+
 app.get("/api/category", (req, res) => {
   res.send(data.categories);
 });
-app.get("/api/products/slug/:slug", (req, res) => {
-  const product = data.products.find((x) => x.slug === req.params.slug);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: "Product Not Found" });
-  }
-});
-app.get("/api/products/:id", (req, res) => {
-  const product = data.products.find((x) => x._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: "Product Not Found" });
-  }
-});
+
+app.use("/api/products", productRouter);
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`serve at http://localhost:${port}`);
